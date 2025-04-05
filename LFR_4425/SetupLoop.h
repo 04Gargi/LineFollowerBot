44a2c2cd6033ccb750b593305299af55858834dc
04Gargi <gargiadeo@gmail.com>
2025-04-05 22:56:39 +0530

    tuned pid . tried to implement junction idea.

--- LFR_4425/SetupLoop.h
+++ LFR_4425/SetupLoop.h
@@ -19,13 +19,32 @@ void s_req() {
   digitalWrite(in3, LOW);
   digitalWrite(in4, LOW);
 
+  pinMode(junctionPulse, INPUT);
+
   delay(3000);
 }
 
 void l_req() {
   lsa_v = L.ReadLSA();
-
   if (lsa_v == 255) stop();
   else moveForward();
+
+  if (digitalRead(junctionPulse)) {
+    if (cnt > 2) {
+      stop();
+
+    } else {
+      moveForward();
+      cnt++;
+    }
+  }
+
+#ifdef pri
+  Serial.print("lsa: ");
+  Serial.print(lsa_v);
+  Serial.print("\t");
+  Serial.print("cnt: ");
+  Serial.print(cnt);
+#endif
 }
 #endif
